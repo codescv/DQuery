ad4404d2eb6be730e4bdae32ef8b68b6d91b0c40
Chi Zhang <elecpaoao@gmail.com>
2016-01-09 23:25:00 +0800

    Initial Commit

---
+++ DQuery/DQuery.h
@@ -0,0 +1,19 @@
+//
+//  DQuery.h
+//  DQuery
+//
+//  Created by Chi Zhang on 1/9/16.
+//  Copyright © 2016 chi zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DQuery.
+FOUNDATION_EXPORT double DQueryVersionNumber;
+
+//! Project version string for DQuery.
+FOUNDATION_EXPORT const unsigned char DQueryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DQuery/PublicHeader.h>
+
+
